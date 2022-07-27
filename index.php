73e6365e71392617fe7698eacaaed865e2318b59
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-07-27 22:07:40 +0200

    favicon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kalkulačka</title>
+    <link rel="icon" href="favicon_io/favicon.ico">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="style.css?v=<?php echo time(); ?> ">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">

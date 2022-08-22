d79712ca95b37f7f67be1f2a2230cbd85edfd668
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-08-22 17:07:28 +0200

    formated

--- math.php
+++ math.php
@@ -1,43 +1,44 @@
 <?php
 $result = null;
-if (isset($_POST['submit'])) {
-$num1=$_POST['number1'];
-$num2=$_POST['number2'];
-$result = ' Vyplńte všechna pole pouze číslem';
+if (isset($_POST['submit']))
+{
+  $num1=$_POST['number1'];
+  $num2=$_POST['number2'];
+  $result = 'Vyplńte všechna pole pouze číslem';
 
- if(is_numeric($num1) && is_numeric($num2)) 
- {
-    if(isset($_POST['g'])) 
-    {
+   if(is_numeric($num1) && is_numeric($num2)) 
+   {
+      if(isset($_POST['g'])) 
+      {
   
-   $operation=$_POST['g'];
+       $operation=$_POST['g'];
    
-   switch ($operation) {
+       switch ($operation) 
+        {
 
-    case '+' ;
-    $result=$num1+$num2;
-    break;
+         case '+' ;
+         $result=$num1+$num2;
+         break;
 
-    case '-' ;
-    $result= $num1-$num2;
-    break;
+         case '-' ;
+         $result= $num1-$num2;
+         break;
 
-    case '*' ;
-    $result= $num1*$num2;
-    break;
+         case '*' ;
+         $result= $num1*$num2;
+         break;
 
-    case '/' ;
-    $result= $num1/$num2;
-    break;
+         case '/' ;
+         $result= $num1/$num2;
+         break;
 
-   }
+        }
+      }
 
+        else 
+        {
+        echo "Prosím, vyberte matematický symbol.";
+        }
     }
 
-    else 
-    {
-      echo "Prosím, vyberte matematický symbol.";
-    }
- }
-
- }
+}

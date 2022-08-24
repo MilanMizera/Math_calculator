65782fee0f3d4cb16f32665afceabbef9cf80154
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-08-24 21:24:50 +0200

    Update math.php

--- math.php
+++ math.php
@@ -5,8 +5,8 @@ if (isset($_POST['submit']))
    $num1=$_POST['number1'];
    $num2=$_POST['number2'];
    $result = 'Vyplńte všechna pole pouze číslem';
-   str_replace(",",".",$num1,$num2);
-
+   str_replace(",",".",$num1);
+   str_replace(",",".",$num2);
    if(is_numeric($num1) && is_numeric($num2)) 
    {
       if(isset($_POST['g'])) 

89d879ca8c6cc09127e3cf38630fd3f566221f58
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-08-24 21:07:12 +0200

    ss

--- math.php
+++ math.php
@@ -2,15 +2,15 @@
 $result = null;
 if (isset($_POST['submit']))
 {
-  $num1=$_POST['number1'];
-  $num2=$_POST['number2'];
-  $result = 'Vyplńte všechna pole pouze číslem';
+   $num1=$_POST['number1'];
+   $num2=$_POST['number2'];
+   $result = 'Vyplńte všechna pole pouze číslem';
+   str_replace(",",".",$num1,$num2);
 
    if(is_numeric($num1) && is_numeric($num2)) 
    {
       if(isset($_POST['g'])) 
       {
-  
        $operation=$_POST['g'];
    
        switch ($operation) 
@@ -34,11 +34,6 @@ if (isset($_POST['submit']))
 
         }
       }
-
-        //else 
-        //{
-        //echo "Prosím, vyberte matematický symbol.";
-        //}
     }
 
 }
